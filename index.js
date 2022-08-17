a72c4a38030b5d9f4e8d742fbdd436a51dfccd9d
Justin White <justinnwhitee@gmail.com>
2022-08-17 18:23:07 -0400

    commit tree 554c5a7693db3b53bcf0eb4d9433328b884ddbab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+    name: "Justin", 
+    streetAddress: "123 Justin Drive",
+};
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    // const newObj = {...object};
+    // newObj[key] = value;
+    // return newObj;
+
+    // -----REFACTORED-----
+    return {
+        ...object,
+        [key]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value){
+    object[key] = value;
+    return object;
+}
+
+function deleteFromEmployeeByKey(object, key) {
+    const newObj = {...object};
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(object, key) {
+    delete object[key];
+    return object;
+}
